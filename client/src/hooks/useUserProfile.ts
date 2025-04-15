ec345e98dd9c10e0aab9869a483e72e0f358e2a5
hristova9 <hristina.hristova@intellias.com>
2025-04-15 15:58:43 +0300

    some changes in the ui

--- client/src/hooks/useUserProfile.ts
+++ client/src/hooks/useUserProfile.ts
@@ -1,5 +1,10 @@
 import { useLocation } from "react-router-dom";
-import { useGetMyselfQuery, useGetUserByIdQuery } from "../store/usersApi";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { useGetUserByIdQuery } from "../store/users/usersApi";
+import { useDispatch } from "react-redux";
+import { setCurrentUser, setLoggedInUser } from "../store/users/usersSlice";
+import { useEffect } from "react";
 
 const useUserProfile = () => {
   const { pathname } = useLocation();
@@ -13,27 +18,31 @@ const useUserProfile = () => {
   } = useGetMyselfQuery();
 
   const {
-    data: userData,
-    error: userError,
-    isLoading: userLoading,
-    refetch: refetchUser,
+    data: currentUser,
+    refetch,
+    isLoading
   } = useGetUserByIdQuery(id as string, {
-    skip: !id || id === "me",
-    refetchOnMountOrArgChange: true,
+    // skip: isMe,
   });
+  console.log(currentUser);
+  
+  useEffect(() => {
+    if (!isMe && currentUser) {
+      dispatch(setCurrentUser(currentUser));
+    }
+  }, [currentUser, dispatch, isMe]);
 
-  const isMe = id === "me";
-  const user = isMe ? currentUserData : userData; 
-  const error = currentUserError || userError;
-  const loading = currentUserLoading || userLoading;
-  const refetch = isMe ? refetchCurrentUser : refetchUser;
-  const isOwnProfile = user && currentUserData && user.id === currentUserData.id;
+  useEffect(() => {
+    if (isMe && currentUser) {
+      dispatch(setLoggedInUser(currentUser));
+    }
+  }, [currentUser, dispatch, isMe]);
 
-  if (error) {
-    return { user: null, loading: false, error: (error as Error).message, isOwnProfile: false };
-  }
+  const user = isMe ? loggedInUser : currentUser;
+  const isOwnProfile = isMe;
+  const loading = !user;
 
-  return { user, loading, error: null, refetch, isOwnProfile: isOwnProfile ?? false };
+  return { user, loading, isOwnProfile, refetch };
 };
 
 export default useUserProfile;
